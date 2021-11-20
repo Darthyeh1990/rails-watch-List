0a8927546953f7a31abeba36ff0002ca405f022b
Timothy Yeh <jcluvt@yahoo.com>
2021-11-20 15:15:47 +0800

    complete controllers

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,30 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save && @bookmark.comment != nil && @bookmark.movie_id != nil
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    @list = List.find(@bookmark[:list_id])
+    redirect_to list_path(@list), notice: 'Bookmark has been removed'
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
